6fc8c609195ca445710f1619f2098ef4c02d6a7b
NandoCruz <nando33_2005@hotmail.com>
2021-04-29 17:58:00 -0300

    primeiro programa de Java

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Fernando");
+    }// Fim do main
+    
+} // Fim da Class
