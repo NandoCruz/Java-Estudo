2e463e784354d082b873104f1357b363b810f0fc
NandoCruz <nando33_2005@hotmail.com>
2021-04-29 17:46:39 -0300

    Aulas iniciais de Java
    Aulas iniciais de Java feitas no curso em vídeo com o professor Gustavo Guanabara.

---
+++ DataHora1/src/datahora/DataHora.java
@@ -0,0 +1,13 @@
+package datahora;
+
+import java.util.Date;
+
+public class DataHora {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
