84886261ddf3596ec014603a42f37c4871d6b5cd
NandoCruz <nando33_2005@hotmail.com>
2021-04-30 11:49:33 -0300

    Operação Aritmética

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author nando
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 5;
+        int n2 = 2;
+        double m = n1 / n2;
+        System.out.println("A média é igual a " + m);
+        
+       /* int num = 10;
+        int valor = 5 + num--;
+        System.out.println("O valor é " + valor);
+        System.out.println("O número vale " + num);*/
+       
+       /* int x=8;
+       x += 2; // x = x + 2
+        System.out.println(x);*/
+       
+       /* float v = 8.4f;
+       int ar = (int) Math.round(v);
+        System.out.println(ar); */
+       /*
+       double ale = Math.random();
+       int n = (int) (15 + ale * (50-15));
+        System.out.println(n);*/
+    }
+    
+}
